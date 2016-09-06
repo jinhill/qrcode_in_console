ee7ba796afa3692c09677c7109818cafc1195700
jinhill <jinhill82@163.com>
2016-09-06 11:26:58 +0800

    update

--- qrcode.c
+++ qrcode.c
@@ -1,19 +1,29 @@
 // QRCode in console
 // Author:Jinhill 2016-9-6 cb@ecd.io
 // Make: gcc -o qrcode qrcode.c -ldl
-
+// depend on libqrencode.so
+//
+// Origin Copyright 2010 Google Inc.
+// Origin Author: Markus Gutschke
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 #include <assert.h>
 #include <errno.h>
 #include <dlfcn.h>
 #include <fcntl.h>
 #include <getopt.h>
-//#include <pwd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include <sys/stat.h>
-//#include <sys/types.h>
-//#include <unistd.h>
 
 static enum { QR_UNSET=0, QR_NONE, QR_ANSI, QR_UTF8 } qr_mode = QR_UNSET;
 
